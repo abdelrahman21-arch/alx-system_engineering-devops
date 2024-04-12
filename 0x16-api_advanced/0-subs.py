8a65fba494a8d78fdde25d28c3793543943a6198
abdelrahman21-arch <boodyhussein12@gmail.com>
2024-04-11 17:48:34 -0700

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
+import json
 
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "Linux:0x16.api.advanced:v1.0.0 (by /u/Neat_Poet_3024)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    try:
+        results = response.json().get("data")
+        if results is None:
+            return (0)
+        return results.get("subscribers")
+    except json.decoder.JSONDecodeError:
+        print("Failed to decode JSON data from response")
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
